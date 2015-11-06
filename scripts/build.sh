83b5b461e9cbc1e0b29656d6f9932c81c42ee318
Marcus Sanatan <msanatan@gmail.com>
2015-11-06 09:51:26 -0400

    Improve minification - JS & HTML
    change variable SI_FILE to have .js extension instead of .txt as we now explicitly list the javascript files to be minified instead of using a directory full of .js files. Also improve HTML minification. Before the process was the remove all whitespace from index.html then add script tag. Now we replace script tags first then minify. The HTML now has no new line characters and everything is separated by one single space character, instead of multiple

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ rm -Rf build
 mkdir build
 # Use an array to preserve order, pretty darn important
 FILES=("javascripts/InputHandler.js" "javascripts/Engine.js" "javascripts/LaserShot.js" "javascripts/Sprite.js" "javascripts/Player.js" "javascripts/Enemy.js" "javascripts/Collision.js" "javascripts/SquareInvaders.js" "javascripts/init.js")
-SI_FILE=javascripts/square-invaders.txt
+SI_FILE=javascripts/square-invaders.js
 touch $SI_FILE
 
 for f in ${FILES[@]}
@@ -14,15 +14,17 @@ done
 
 echo "Done!"
 echo "Minifying JavaScript files..."
-mv $SI_FILE "./javascripts/square-invaders.js"
-minify "./javascripts/square-invaders.js" -o build/square-invaders.min.js
-rm "./javascripts/square-invaders.js"
+minify $SI_FILE -o build/square-invaders.min.js
+rm $SI_FILE
 
-echo "\"Minify\" HTML as well"
+"Creating HTML"
 HTML_FILE=build/index.html
-tr -d "\011\012\015" < index.html | cat > $HTML_FILE
+touch $HTML_FILE
+cat index.html > $HTML_FILE
 echo "Removing old script references"
 perl -i.bak -0777ne 's|<script.*?</script>||gms;print' $HTML_FILE
 sed -i ".bak" s/'<\/body>'/"<script type='application\/javascript' src='square-invaders.min.js'><\/script><\/body>"/ $HTML_FILE
+echo "\"Minify\" HTML as well"
+cat $HTML_FILE | tr -d "\n" |  tr -s " "  > $HTML_FILE
 rm build/index.html.bak
 echo "Build complete :)"
