5cba82863713e08999b569346cef7a31d0d2b612
Marcus Sanatan <msanatan@gmail.com>
2015-11-06 10:11:31 -0400

    Improve minification - remove newlines in js file and replace with semicolon

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,9 @@ mkdir build
 # Use an array to preserve order, pretty darn important
 FILES=("javascripts/InputHandler.js" "javascripts/Engine.js" "javascripts/LaserShot.js" "javascripts/Sprite.js" "javascripts/Player.js" "javascripts/Enemy.js" "javascripts/Collision.js" "javascripts/SquareInvaders.js" "javascripts/init.js")
 SI_FILE=javascripts/square-invaders.js
-touch $SI_FILE
+SI_BUILD=build/square-invaders.min.js
+HTML_FILE=build/index.html
+touch $SI_FILE && touch $SI_BUILD && touch $HTML_FILE
 
 for f in ${FILES[@]}
 do
@@ -14,17 +16,16 @@ done
 
 echo "Done!"
 echo "Minifying JavaScript files..."
-minify $SI_FILE -o build/square-invaders.min.js
+minify $SI_FILE -o $SI_BUILD
+cat $SI_BUILD | tr "\n" ";" > temp.txt && mv temp.txt $SI_BUILD
 rm $SI_FILE
 
-"Creating HTML"
-HTML_FILE=build/index.html
-touch $HTML_FILE
+echo "Creating HTML"
 cat index.html > $HTML_FILE
 echo "Removing old script references"
 perl -i.bak -0777ne 's|<script.*?</script>||gms;print' $HTML_FILE
 sed -i ".bak" s/'<\/body>'/"<script type='application\/javascript' src='square-invaders.min.js'><\/script><\/body>"/ $HTML_FILE
 echo "\"Minify\" HTML as well"
-cat $HTML_FILE | tr -d "\n" |  tr -s " "  > $HTML_FILE
+cat $HTML_FILE | tr -d "\n" |  tr -s " " > temp.txt && mv temp.txt $HTML_FILE
 rm build/index.html.bak
 echo "Build complete :)"
