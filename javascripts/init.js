567514db93d6c4d2955314cd500098b2783f59ce
Marcus Sanatan <msanatan@gmail.com>
2015-11-06 11:22:17 -0400

    Clean up variables - initialise and define globals at once

--- javascripts/init.js
+++ javascripts/init.js
@@ -1,19 +1,16 @@
 var animate;
 var Game = Game || {};
-var options = {};
-var engine;
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
 var FPS = 30;
+var engine = new Game.Engine('game', WIDTH, HEIGHT, FPS);
 
-engine = new Game.Engine('game', WIDTH, HEIGHT, FPS);
 engine.init();
-options = {
+engine.register(new Game.SquareInvaders({
   width: WIDTH,
   height: HEIGHT,
   FPS: FPS
-};
-engine.register(new Game.SquareInvaders(options));
+}));
 
 // Use fallbacks for requestAnimationFrame
 animate = (window.requestAnimationFrame ||
