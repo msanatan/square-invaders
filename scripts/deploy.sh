0c421a252902291892ab80da3f945846e3ab3857
Marcus Sanatan <msanatan@gmail.com>
2015-11-09 21:43:30 -0400

    Commit message now includes the last commit of the master branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+commit=$(git rev-parse HEAD)
 cd build
 git init
 git checkout -b gh-pages
 git add .
-git commit -am "New build by ${USER}"
+git commit -am "New build by ${USER}, last commit: $commit"
 git push "git@github.com:msanatan/square-invaders.git" gh-pages:gh-pages --force
 rm -Rf .git
